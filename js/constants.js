41f12edfcb1c2425f011f37f0e8b5644bd05de7b
Andjohn <30844240+andiohn@users.noreply.github.com>
2026-03-26 16:13:36 -0600

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,11 @@
 export const GRID_COLS = 22;
 export const GRID_ROWS = 5;
 
-export const SCRAMBLE_DURATION = 800;
-export const FLIP_DURATION = 300;
+export const SCRAMBLE_DURATION = 8000;
+export const FLIP_DURATION = 3000;
 export const STAGGER_DELAY = 25;
 export const TOTAL_TRANSITION = 3800;
-export const MESSAGE_INTERVAL = 4000;
+export const MESSAGE_INTERVAL = 40000;
 
 export const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?\'/: ';
 
